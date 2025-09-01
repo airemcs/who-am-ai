6afb2a53e44aafe8e1b927f6d0492223293de19a
airemcs <its.airemcs@gmail.com>
2025-09-01 14:05:41 +0800

    Incorporate Screen Size Breakpoints

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+// Desktop: 1440px -> 1200px    Margin  = 120px
+// Tablet: 830px                        = 80-56px
+// Mobile: 393px                        = 20-16px
+
+// H1: 80 -> 64 -> 44
+// H2: 56 -> 56 -> 32
+// H3: 32 -> 32 -> 28
+// H4: 20 -> 20 -> 18
+
 export default function App() {
   return (
   <h1 className="text-lg font-inter">
