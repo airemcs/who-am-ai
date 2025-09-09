c72b46439bdda4e922cfba08a03f6e49c4355356
airemcs <its.airemcs@gmail.com>
2025-09-09 11:18:32 +0800

    Transfer Breakpoints

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-// Desktop: 1440px -> 1200px    Margin  = 120px
-// Tablet: 830px                        = 80-56px
-// Mobile: 393px                        = 20-16px
-
-// H1: 80 -> 64 -> 44
-// H2: 56 -> 56 -> 32
-// H3: 32 -> 32 -> 28
-// H4: 20 -> 20 -> 18
-
 export default function App() {
+
+  // Desktop: 1440px -> 1200px    Margin  = 120px
+  // Tablet: 830px                        = 80-56px
+  // Mobile: 393px                        = 20-16px
+
+  // H1: 80 -> 64 -> 44
+  // H2: 56 -> 56 -> 32
+  // H3: 32 -> 32 -> 28
+  // H4: 20 -> 20 -> 18
+
   return (
   <h1 className="text-lg font-inter">
     Hello World!
