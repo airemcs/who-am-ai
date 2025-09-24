92863c1a0265a21fc7ac72a6d94c0601fde0e7e5
airemcs <its.airemcs@gmail.com>
2025-09-24 16:44:38 +0800

    Create Login Page

---
+++ src/Login.tsx
@@ -0,0 +1,14 @@
+import { AnimatedGridPattern } from "@/components/magicui/animated-grid-pattern";
+import LoginCard from "./components/login/LoginCard";
+
+export default function Login() {
+  return (
+  <div className="relative flex h-screen w-full items-center justify-center overflow-hidden">
+
+    <AnimatedGridPattern numSquares={50} width={25} height={25} maxOpacity={0.25} duration={5} repeatDelay={1} className="absolute inset-0 skew-y-12 scale-150 opacity-50 [mask-image:radial-gradient(1200px_circle_at_center,white,transparent)]" />
+
+    <LoginCard />
+    
+  </div>
+  )
+}
